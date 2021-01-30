1c089588b5d775f70fc22474c32a01359c9b20d6
gascraft971 <gascraft971@gmail.com>
2021-01-30 16:34:31 +0100

    Filled config and added init from config

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import RPi.GPIO as GPIO
 from classes import Robot
 import json
 
-with open("config.json") as file:
-	pins = json.load(file)
-
 if __name__ == "__main__":
-	robot = Robot(GPIO.BCM, pins)
+	with open("config.json") as configFile:
+		config = json.load(configFile)
+	
+	pins = config["pins"]
+	print(pins)
+
+	robot = Robot.Robot(GPIO.BCM, pins)
